98f012eda221da4d856dd5f66c3d8e751bb6712b
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2020-11-20 00:44:05 +0300

    Update alistirma5.py

--- alistirma5.py
+++ alistirma5.py
@@ -2,3 +2,8 @@ for i in range(1000,10000):
      if str(i)[0] == str(i)[-1] and str(i)[1] == str(i)[-2]:
          print(i)
     
+#liste=[]
+#for i in range(1000,10000):
+#    if str(i)[0]==str(i)[3] and str(i)[1]==str(i)[2]:
+#        liste.append(i)
+#print(liste)
